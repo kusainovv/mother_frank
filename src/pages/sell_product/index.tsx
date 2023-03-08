efe6282aa3aa49ca76b2fcfaa2688f9ca6944d1a
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-03-09 02:37:08 +0400

    add step by step ui for sell product --feature --refactoring --styling

--- src/pages/sell_product/index.tsx
+++ src/pages/sell_product/index.tsx
@@ -15,20 +15,45 @@ const SellProduct = () => {
             </div>
 
 
-            <p className={style['Title']}>Сделать заявку</p>
-                <div className={style['TelegramMessagePlaceholder']}>
-                    <div className={style['TelegramMessageContent']}>
-                        <p> 1. Название товара </p>
-                        <p> 2. Цена товара </p>
-                        <p> 3. Размер/Размеры </p>
-                        <p> 4. Телеграм id </p>
-                        <p> 5. О товаре </p>
-                        <p> 6. Рекомендации по уходу </p>
-                        <p> 7. Размерная сетка </p>
-                        <p> 8. Дополнительная информация о товаре </p>
-                    </div>
+            <p className={style['Title']} id='message_template'>Шаблон заявки</p>
+            <div className={style['TelegramMessagePlaceholder']}>
+                <div className={style['TelegramMessageContent']}>
+                    <p> 1. Название товара </p>
+                    <p> 2. Цена товара </p>
+                    <p> 3. Размер/Размеры </p>
+                    <p> 4. Телеграм id </p>
+                    <p> 5. О товаре </p>
+                    <p> 6. Рекомендации по уходу </p>
+                    <p> 7. Размерная сетка </p>
+                    <p> 8. Дополнительная информация о товаре </p>
                 </div>
+            </div>
+
+            <p className={style['Title']}>Как выставить товар (по шагам)</p>
+            <div className={style['Steps']}>
+                <div className={style['Step']}>
+                    1. Напишите любое сообщение в телеграм <a href='' className={style['RuleLink']}>@mother_frank</a>
+                </div>
+                <div className={style['VerticalLine']} />
+                <div className={style['Step']}>
+                    {'2. Ждите когда бот напишет "Я готов вас слушать"'}
+                </div>
+                <div className={style['VerticalLine']} />
+                <div className={style['Step']}>
+                    3. Отправьте ему сообщением информацию о вашем продукте (см. <a href='#message_template' className={style['RuleLink']}>Шаблон заявки</a>)
+                    <br />
+                    <p className={style['StepImages']}>
+                        В одном сообщении с информации о вашем продукте, добавьте как минимум одно изображение вашей продукции
+                    </p>
+                </div>
+                <div className={style['VerticalLine']} />
+                <div className={style['Step']}>
+                    4. Ждите когда модератор обработает ваш товар (1/2 дня)
+                </div>
+            </div>
         </div>
+
+
 
         <Footer />
     </div>
